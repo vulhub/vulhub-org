0377666f33c70dcd873eb069dc5a80d30823e6d6
phith0n <phith0n.ph2f@gmail.com>
2017-10-21 06:59:16 +0800

    implement .startsWith in IE

--- src/index.js
+++ src/index.js
@@ -19,6 +19,12 @@ app({
     addEventListener("popstate", () => {
       actions.init()
     })
+    if (!String.prototype.startsWith) {
+      String.prototype.startsWith = function(searchString, position) {
+        position = position || 0;
+        return this.indexOf(searchString, position) === position;
+      }
+    }
   },
   state: {
     repositories: repositories,
