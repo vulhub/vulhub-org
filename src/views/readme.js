5092b519c8a2428cf9e4bf5dc28ef1cbab3c1be1
phith0n <phith0n.ph2f@gmail.com>
2019-05-17 11:34:59 +0800

    upgrade statistic

--- src/views/readme.js
+++ src/views/readme.js
@@ -29,19 +29,19 @@ export const Readme = ({state, actions}, children) => {
                                 <div class="level-item has-text-centered">
                                     <div>
                                     <p class="heading">PULL</p>
-                                    <p class="title">30K+</p>
+                                    <p class="title">50K+</p>
                                     </div>
                                 </div>
                                 <div class="level-item has-text-centered">
                                     <div>
                                     <p class="heading">COMMITS</p>
-                                    <p class="title">1000+</p>
+                                    <p class="title">1100+</p>
                                     </div>
                                 </div>
                                 <div class="level-item has-text-centered">
                                     <div>
                                     <p class="heading">STARS</p>
-                                    <p class="title">3000+</p>
+                                    <p class="title">3400+</p>
                                     </div>
                                 </div>
                             </nav>
