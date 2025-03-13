e93f5e70d9d761454b97655565a7b6ff320052cc
phith0n <phith0n.ph2f@gmail.com>
2025-03-13 11:03:27 +0800

    make compatible with Cloudflare pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
-let userConfig = undefined
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
+let userConfig = undefined;
 try {
-  userConfig = await import('./v0-user-next.config')
+  userConfig = await import("./v0-user-next.config");
 } catch (e) {
   // ignore error
 }
@@ -21,28 +23,32 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
-}
+};
 
-mergeConfig(nextConfig, userConfig)
+mergeConfig(nextConfig, userConfig);
 
 function mergeConfig(nextConfig, userConfig) {
   if (!userConfig) {
-    return
+    return;
   }
 
   for (const key in userConfig) {
     if (
-      typeof nextConfig[key] === 'object' &&
+      typeof nextConfig[key] === "object" &&
       !Array.isArray(nextConfig[key])
     ) {
       nextConfig[key] = {
         ...nextConfig[key],
         ...userConfig[key],
-      }
+      };
     } else {
-      nextConfig[key] = userConfig[key]
+      nextConfig[key] = userConfig[key];
     }
   }
 }
 
-export default nextConfig
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
+
+export default nextConfig;
