0a7520abe2eecf4b8358a760ce49ada1db3afe6d
phith0n <phith0n.ph2f@gmail.com>
2025-04-20 21:33:02 +0800

    edge runtime

--- app/api/statistic/route.ts
+++ app/api/statistic/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { getAllEnvironments } from '@/lib/environments';
 
+export const runtime = 'edge';
+
 export async function GET() {
   const environments = getAllEnvironments();
   return NextResponse.json({ environments: environments.length });
